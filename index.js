a426fcee9e9c44bf7cb05b347af4f0558b327c59
jkdrangel <jkdrangel@hotmail.com>
2017-10-26 09:23:31 -0300

    Add hasPadding prop and icons

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {
     Animated,
     Dimensions,
     TouchableOpacity,
-    Easing
+    Easing,
+    Image
 } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
@@ -47,16 +48,19 @@ export default class SwitchSelector extends Component {
             fontSize,
             buttonColor,
             backgroundColor,
-            borderColor
+            borderColor,
+            hasPadding
         } = this.props;
 
         const left = this.animatedValue.interpolate({
             inputRange: [0, 1],
-            outputRange: [4, this.state.sliderWidth - 3]
+            outputRange: [(hasPadding?3:0), this.state.sliderWidth - (hasPadding?3:0)]
         });
         const options = this.props.options.map((element, index) =>
             <View key={index} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
-                <TouchableOpacity hitSlop={{ top: 30, bottom: 30, left: 10, right: 10 }} onPress={() => this.SwitchSelector(index)}>
+                <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} hitSlop={{ top: 30, bottom: 30, left: 10, right: 10 }} onPress={() => this.SwitchSelector(index)}>
+                    {element.customIcon}
+                    {element.imageIcon && <Image source={element.imageIcon} style={{ height: 30, width: 30, tintColor: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000' }} />}
                     <Text style={{ fontSize, textAlign: 'center', color: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000', backgroundColor: 'transparent' }}>{element.label}</Text>
                 </TouchableOpacity>
             </View>
@@ -68,17 +72,17 @@ export default class SwitchSelector extends Component {
                     <View style={{ borderRadius: 50, backgroundColor: backgroundColor || '#ffffff', height: 40 }}
                         onLayout={(event) => {
                             var { x, y, width, height } = event.nativeEvent.layout;
-                            this.setState({ sliderWidth: (width - 3) });
+                            this.setState({ sliderWidth: (width - (hasPadding?3:0)) });
                         }}>
-                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', borderColor: borderColor || '#c9c9c9', borderRadius: 50, borderWidth: 1 }}>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: 1 }}>
                             <Animated.View ref="switcher" style={{
                                 borderRadius: 50,
                                 borderWidth: 0,
-                                height: 34,
-                                backgroundColor: buttonColor || '#BCD635', width: (this.state.sliderWidth / this.props.options.length) - 3,
+                                height: (hasPadding?34:40),
+                                backgroundColor: buttonColor || '#BCD635', width: (this.state.sliderWidth / this.props.options.length) - (hasPadding?2:0),
                                 position: 'absolute',
                                 left: left,
-                                marginTop: 2
+                                marginTop: (hasPadding?2:0)
                             }} />
                             {options}
                         </View>
