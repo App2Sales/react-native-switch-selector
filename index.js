a0b585d3028328edaec380161b2720c9f98e9ad2
jkdrangel <jkdrangel@hotmail.com>
2017-12-27 14:16:06 -0300

    Add active color to option

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StyleSheet,
     Animated,
-    Dimensions,
     TouchableOpacity,
     Easing,
     Image
 } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
-
 export default class SwitchSelector extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            selected: this.props.initial ? this.props.initial : 0,
-        }
-        this.animatedValue = new Animated.Value(this.props.initial ? (this.props.initial / this.props.options.length) : 0)
+            selected: this.props.initial ? this.props.initial : 0
+        };
+        this.animatedValue = new Animated
+            .Value(this.props.initial ? (this.props.initial / this.props.options.length) : 0);
+    }
+
+    getBgColor() {
+        const { selected } = this.state;
+        const { options, buttonColor } = this.props;
+        return options[selected].activeColor || buttonColor;
     }
 
     animate = (value, last) => {
@@ -36,11 +38,14 @@ export default class SwitchSelector extends Component {
 
     SwitchSelector = (index) => {
         if (this.props.options.length <= 1) return;
-        this.animate(index / this.props.options.length, this.state.selected / this.props.options.length);
+        this.animate(
+            index / this.props.options.length,
+            this.state.selected / this.props.options.length
+        );
         if (this.props.onPress) {
             this.props.onPress(this.props.options[index].value);
         } else {
-            console.log("Call onPress with value: ", this.props.options[index].value)
+            console.log('Call onPress with value: ', this.props.options[index].value);
         }
         this.setState({ selected: index });
     }
@@ -50,7 +55,6 @@ export default class SwitchSelector extends Component {
             textColor,
             selectedColor,
             fontSize,
-            buttonColor,
             backgroundColor,
             borderColor,
             hasPadding
@@ -61,33 +65,42 @@ export default class SwitchSelector extends Component {
             outputRange: [(hasPadding ? 3 : 0), this.state.sliderWidth - (hasPadding ? 3 : 0)]
         });
         const options = this.props.options.map((element, index) =>
-            <View key={index} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
-                <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} hitSlop={{ left: 10, right: 10 }} onPress={() => this.SwitchSelector(index)}>
-                    {element.customIcon}
-                    {element.imageIcon && <Image source={element.imageIcon} style={{ height: 30, width: 30, tintColor: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000' }} />}
-                    <Text style={{ fontSize, textAlign: 'center', color: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000', backgroundColor: 'transparent' }}>{element.label}</Text>
-                </TouchableOpacity>
-            </View>
-        )
+            (
+                <View key={index} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                    <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} hitSlop={{ left: 10, right: 10 }} onPress={() => this.SwitchSelector(index)}>
+                        {element.customIcon}
+                        {element.imageIcon && <Image source={element.imageIcon} style={{ height: 30, width: 30, tintColor: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000' }} />}
+                        <Text style={{
+                            fontSize, textAlign: 'center', color: this.state.selected == index ? selectedColor || '#fff' : textColor || '#000', backgroundColor: 'transparent'
+                        }}>{element.label}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            ));
 
         return (
             <View style={{ flexDirection: 'row' }}>
                 <View style={{ flex: 1 }}>
-                    <View style={{ borderRadius: 50, backgroundColor: backgroundColor || '#ffffff', height: 40 }}
+                    <View
+                        style={{ borderRadius: 50, backgroundColor: backgroundColor || '#ffffff', height: 40 }}
                         onLayout={(event) => {
-                            var { x, y, width, height } = event.nativeEvent.layout;
+                            const { width } = event.nativeEvent.layout;
                             this.setState({ sliderWidth: (width - (hasPadding ? 3 : 0)) });
                         }}>
-                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: 1 }}>
-                            <Animated.View ref="switcher" style={{
-                                borderRadius: 50,
-                                borderWidth: 0,
-                                height: (hasPadding ? 34 : 40),
-                                backgroundColor: buttonColor || '#BCD635', width: (this.state.sliderWidth / this.props.options.length) - (hasPadding ? 2 : 0),
-                                position: 'absolute',
-                                left: left,
-                                marginTop: (hasPadding ? 2 : 0)
-                            }} />
+                        <View style={{
+                            flex: 1, flexDirection: 'row', justifyContent: 'center', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: 1
+                        }}>
+                            <Animated.View
+                                style={{
+                                    borderRadius: 50,
+                                    borderWidth: 0,
+                                    height: (hasPadding ? 34 : 40),
+                                    backgroundColor: this.getBgColor() || '#BCD635',
+                                    width: (this.state.sliderWidth / this.props.options.length) - (hasPadding ? 2 : 0),
+                                    position: 'absolute',
+                                    left,
+                                    marginTop: (hasPadding ? 2 : 0)
+                                }} />
                             {options}
                         </View>
                     </View>
@@ -96,12 +109,3 @@ export default class SwitchSelector extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-});
