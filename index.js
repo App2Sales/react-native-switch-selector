009c369c2b4828e1438a2985ac28350bef467660
jkdrangel <jkdrangel@hotmail.com>
2018-01-09 10:45:07 -0300

    Fix iOS Style

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {
 } from 'react-native';
 
 const styles = {
-    button: { flex: 1, flexDirection: 'column' },
+    button: { flexDirection: 'row', justifyContent: 'center', alignItems: 'center' },
     containerButton: {
         flexDirection: 'row', flex: 1, height: 40, justifyContent: 'center', alignItems: 'center'
     },
@@ -78,34 +78,42 @@ export default class SwitchSelector extends Component {
         });
         const options = this.props.options.map((element, index) =>
             (
-                <TouchableOpacity key={index} style={styles.button} onPress={() => this.toggleItem(index)}>
-                    <View style={styles.containerButton}>
+                <View key={index} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                    <TouchableOpacity style={styles.button} onPress={() => this.toggleItem(index)}>
                         {element.customIcon}
                         {element.imageIcon && <Image source={element.imageIcon} style={{ height: 30, width: 30, tintColor: this.state.selected == index ? selectedColor : textColor }} />}
                         <Text style={{
                             fontSize, textAlign: 'center', color: this.state.selected == index ? selectedColor : textColor, backgroundColor: 'transparent'
                         }}>{element.label}
                         </Text>
-                    </View>
-                </TouchableOpacity>
+                    </TouchableOpacity>
+                </View>
             ));
 
         return (
-            <View
-                style={{ borderWidth: hasPadding ? 1 : 0, borderColor: borderColor, flex: 1, flexDirection: 'row', justifyContent: 'center', borderRadius: 50, backgroundColor: backgroundColor, height: 40 }}
-                onLayout={(event) => {
-                    const { width } = event.nativeEvent.layout;
-                    this.setState({ sliderWidth: (width - (hasPadding ? 3 : 0)) });
-                }}>
-                <Animated.View
-                    style={[{
-                        height: (hasPadding ? 34 : 40),
-                        backgroundColor: this.getBgColor(),
-                        width: (this.state.sliderWidth / this.props.options.length) - (hasPadding ? 2 : 0),
-                        left,
-                        marginTop: (hasPadding ? 2 : 0)
-                    }, styles.animated]} />
-                {options}
+            <View style={{ flexDirection: 'row' }}>
+                <View style={{ flex: 1 }}>
+                    <View
+                        style={{ borderRadius: 50, backgroundColor: backgroundColor, height: 40 }}
+                        onLayout={(event) => {
+                            const { width } = event.nativeEvent.layout;
+                            this.setState({ sliderWidth: (width - (hasPadding ? 3 : 0)) });
+                        }}>
+                        <View style={{
+                            flex: 1, flexDirection: 'row', justifyContent: 'center', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: hasPadding ? 1 : 0
+                        }}>
+                            <Animated.View
+                                style={[{
+                                    height: (hasPadding ? 34 : 40),
+                                    backgroundColor: this.getBgColor(),
+                                    width: (this.state.sliderWidth / this.props.options.length) - (hasPadding ? 2 : 0),
+                                    left,
+                                    marginTop: (hasPadding ? 2 : 0)
+                                }, styles.animated]} />
+                            {options}
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
