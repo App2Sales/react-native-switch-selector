bf1210dd9efaa7df5bfca419da9ca521eb94cca1
jkdrangel <jkdrangel@hotmail.com>
2018-02-01 14:57:30 -0300

    Add pan responder to get gesture swipe

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {
     TouchableOpacity,
     Easing,
     Image,
-    I18nManager
+    I18nManager,
+    PanResponder
 } from 'react-native';
 
 const styles = {
@@ -29,6 +30,40 @@ export default class SwitchSelector extends Component {
         };
         this.animatedValue = new Animated
             .Value(this.props.initial ? (I18nManager.isRTL ? -(this.props.initial / this.props.options.length) : (this.props.initial / this.props.options.length)) : 0);
+    }
+
+    componentWillMount() {
+        this._panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: this.shouldSetResponder,
+            onMoveShouldSetPanResponder: this.shouldSetResponder,
+            onPanResponderRelease: this.responderEnd,
+            onPanResponderTerminate: this.responderEnd
+        });
+    }
+
+    shouldSetResponder = (evt, gestureState) => {
+        return evt.nativeEvent.touches.length === 1 &&
+            !(Math.abs(gestureState.dx) < 5 && Math.abs(gestureState.dy) < 5);
+    }
+
+    responderEnd = (evt, gestureState) => {
+        const swipeDirection = this._getSwipeDirection(gestureState);
+        if (swipeDirection === 'RIGHT' && this.state.selected < (this.props.options.length - 1)) {
+            this.toggleItem(this.state.selected + 1)
+        } else if (swipeDirection === 'LEFT' && this.state.selected > 0) {
+            this.toggleItem(this.state.selected - 1)
+        }
+    }
+
+    _getSwipeDirection(gestureState) {
+        const { dx, dy, vx } = gestureState;
+        // 0.1 velocity
+        if (Math.abs(vx) > 0.1 && Math.abs(dy) < 80) {
+            return (dx > 0)
+                ? 'RIGHT'
+                : 'LEFT';
+        }
+        return null;
     }
 
     getBgColor() {
@@ -76,7 +111,9 @@ export default class SwitchSelector extends Component {
 
         const options = this.props.options.map((element, index) =>
             (
-                <View key={index} style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                <View
+                    key={index}
+                    style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
                     <TouchableOpacity style={styles.button} onPress={() => this.toggleItem(index)}>
                         {element.customIcon}
                         {element.imageIcon && <Image source={element.imageIcon} style={{ height: 30, width: 30, tintColor: this.state.selected == index ? selectedColor : textColor }} />}
@@ -90,42 +127,45 @@ export default class SwitchSelector extends Component {
 
         return (
             <View style={{ flexDirection: 'row' }}>
-                <View style={{ flex: 1 }}>
+                <View
+                    {...this._panResponder.panHandlers}
+                    style={{ flex: 1 }}>
                     <View
                         style={{ borderRadius: 50, backgroundColor: backgroundColor, height: 40 }}
                         onLayout={(event) => {
                             const { width } = event.nativeEvent.layout;
                             this.setState({ sliderWidth: (width - (hasPadding ? 2 : 0)) });
                         }}>
-                        <View style={{
-                            flex: 1, flexDirection: 'row', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: hasPadding ? 1 : 0
-                        }}>
-                        {this.state.sliderWidth && (
-                            <Animated.View
-                              style={[
-                                {
-                                  height: hasPadding ? 34 : 40,
-                                  backgroundColor: this.getBgColor(),
-                                  width:
-                                    this.state.sliderWidth / this.props.options.length - (hasPadding ? 2 : 0),
-                                  transform: [
-                                    {
-                                      translateX: this.animatedValue.interpolate({
-                                        inputRange: [0, 1],
-                                        outputRange: [
-                                          hasPadding ? 2 : 0,
-                                          this.state.sliderWidth - (hasPadding ? 2 : 0),
-                                        ],
-                                      }),
-                                    },
-                                  ],
-                                  marginTop: hasPadding ? 2 : 0,
-                                },
-                                styles.animated,
-                              ]}
-                            />
-                          )}
-                          {options}
+                        <View
+                            style={{
+                                flex: 1, flexDirection: 'row', borderColor: borderColor || '#c9c9c9', borderRadius: 60, borderWidth: hasPadding ? 1 : 0
+                            }}>
+                            {this.state.sliderWidth && (
+                                <Animated.View
+                                    style={[
+                                        {
+                                            height: hasPadding ? 34 : 40,
+                                            backgroundColor: this.getBgColor(),
+                                            width:
+                                                this.state.sliderWidth / this.props.options.length - (hasPadding ? 2 : 0),
+                                            transform: [
+                                                {
+                                                    translateX: this.animatedValue.interpolate({
+                                                        inputRange: [0, 1],
+                                                        outputRange: [
+                                                            hasPadding ? 2 : 0,
+                                                            this.state.sliderWidth - (hasPadding ? 2 : 0),
+                                                        ],
+                                                    }),
+                                                },
+                                            ],
+                                            marginTop: hasPadding ? 2 : 0,
+                                        },
+                                        styles.animated,
+                                    ]}
+                                />
+                            )}
+                            {options}
                         </View>
                     </View>
                 </View>
